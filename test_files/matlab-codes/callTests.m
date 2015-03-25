3c29427e0ec2bb916a6307456650acdefc80f9fb
trixr4kdz <troque@lion.lmu.edu>
2015-03-24 23:46:14 -0700

    Added more tests

---
+++ test_files/matlab-codes/callTests.m
@@ -0,0 +1,7 @@
+clear all
+clc
+
+% Adds all folders and subfolders in the Users directory
+% Not very efficient but will search all directories under Users/GRNmap
+addpath(genpath('C:\Users\GRNmap\matlab'));
+run(readInputSheetTest);
