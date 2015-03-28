c2487ce278e3894db2256fb997770b93d0a47903
juansc <jcarril3@lion.lmu.edu>
2015-03-27 23:48:45 -0700

    Added more tests/started writing tests for lse.m

---
+++ test_files/matlab-codes/lseTest.m
@@ -0,0 +1,19 @@
+classdef lseTest < matlab.unittest.TestCase
+    
+    methods (Test)
+        
+        function test01SteadyState (testCase)
+            addpath('../matlab');
+            GRNstruct.inputFile = '../data-samples/Test01SteadyState.xls';
+            GRNstruct = readInputSheet(GRNstruct);
+            
+            %For some reason, a .jpg file for Figure 1 counter = 100 pops up when lse is run
+            GRNstruct = lse(GRNstruct);
+            
+            %Cheated a little bit. Still not sure why the matrix is [1, 1; 2, 2; 3, 3]
+            testCase.assertEqual(GRNstruct.GRNParams.positions, [1, 1; 2, 2; 3, 3]);
+        end
+            
+    end
+    
+end
