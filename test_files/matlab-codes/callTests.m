e29df05693a0aae959310866255d7abaa869d00b
trixr4kdz <troque@lion.lmu.edu>
2015-04-25 17:48:57 -0700

    Removed unnecessary files

--- test_files/matlab-codes/callTests.m
+++
@@ -1,9 +0,0 @@
-clear all
-clc
-
-%Adds necessary directories to search path
-addpath(('..\..\matlab'));
-
-%Begin running tests
-readInputSheetTestResult = run(readInputSheetTest);
-lseTestResult = run(lseTest);
