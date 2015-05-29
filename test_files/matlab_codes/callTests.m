994ea184a5d62049e3ffaa0a5d39a5e4a1eea8f5
trixr4kdz <troque@lion.lmu.edu>
2015-05-28 17:13:58 -0700

    Fixing up tests to check if all worksheets in an Excel file are present

--- test_files/matlab_codes/callTests.m
+++ test_files/matlab_codes/callTests.m
@@ -2,7 +2,16 @@ clear all
 clc
 
 %Adds necessary directories to search path
-addpath(('..\..\matlab'));
+path = which('exampleFunctionTest.m');
+[p, name, ext] = fileparts(path);
+p = strrep(p, 'matlab_codes', '');
+addpath(strcat(p, 'estimation_tests'));
+
+% filename = '3-genes_3-edges_artificial-data_Sigmoid_estimation_1.xls';
+% inputFile = which(filename);
+% [A, B] = xlsfinfo(inputFile);
+
+%[path, name, ext] = fileparts(filename)
 
 %Begin running tests
 readInputSheetTestResult = run(readInputSheetTest);
