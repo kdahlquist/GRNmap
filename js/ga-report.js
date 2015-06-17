af4c0b6e01feb82df7c1e33759f234af1714f2b0
dondi <dondi@lmu.edu>
2015-06-17 13:25:59 -0700

    Fix Google Analytics URL.

--- js/ga-report.js
+++ js/ga-report.js
@@ -1,5 +1,5 @@
 $(function () {
-    $.getJSON("http://grnsight.cs.lmu.edu/server/beta/grnmap?path=" + location.pathname.split("/").pop(), function (result) {
+    $.getJSON("http://grnsight.cs.lmu.edu/beta/server/grnmap?path=" + location.pathname.split("/").pop(), function (result) {
         $(".ga-report").text(result);
     });
 });
