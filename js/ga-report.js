accab3309f1393ff839fe41023095a21850a5f3a
Trixie Roque <troque@lion.lmu.edu>
2015-06-17 16:59:37 -0700

    Attempt at implementing download counter to Downloads Page

--- js/ga-report.js
+++ js/ga-report.js
@@ -1,5 +1,13 @@
 $(function () {
     $.getJSON("http://grnsight.cs.lmu.edu/beta/server/grnmap?path=" + location.pathname.split("/").pop(), function (result) {
+        
+        if(location.pathname.split("/").pop() === "downloads.html") {
+            $.getJSON("http://grnsight.cs.lmu.edu/beta/server/grnmap?path=download", function (uploadCount) {
+                $(".ga-download").text(downloadCount);
+            })
+        }
+        
         $(".ga-report").text(result);
+        
     });
 });
