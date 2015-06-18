d09deb909a24f3d7a03d17465d25b556bfd81ccc
Trixie Roque <troque@lion.lmu.edu>
2015-06-18 01:41:46 -0700

    Fixed Download pageview

--- js/ga-report.js
+++ js/ga-report.js
@@ -2,14 +2,15 @@ $(function () {
     $.getJSON("http://grnsight.cs.lmu.edu/beta/server/grnmap?path=" + location.pathname.split("/").pop(), function (result) {
         
         if (location.pathname.split("/").pop() === "downloads.html") {
-            var exec = $(this.attr('class')).closest('div[id$="_executable"]').attr('id');
-            var source = $(this.attr('class')).closest('div[id$="_source"]').attr('id');
+            var exec = $(this).closest('div[id$="_executable"]').attr('id');
+            var source = $(this).closest('div[id$="_source"]').attr('id');
             $.getJSON("http://grnsight.cs.lmu.edu/beta/server/grnmap?path=download" + "/" + exec, function (execDownloadCount) {
                 $(".ga-execDownload").text(execDownloadCount);
             })
             $.getJSON("http://grnsight.cs.lmu.edu/beta/server/grnmap?path=download" + "/" + source, function (sourceDownloadCount) {
                 $(".ga-sourceDownload").text(sourceDownloadCount);
             })
+            
         }
 
         $(".ga-report").text(result);
