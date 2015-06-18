9d1a6dfc4aea69e2028f0f3130c6ade1674e77c0
Trixie Roque <troque@lion.lmu.edu>
2015-06-17 17:02:29 -0700

    Attempt no. 2

--- js/ga-report.js
+++ js/ga-report.js
@@ -2,7 +2,7 @@ $(function () {
     $.getJSON("http://grnsight.cs.lmu.edu/beta/server/grnmap?path=" + location.pathname.split("/").pop(), function (result) {
         
         if(location.pathname.split("/").pop() === "downloads.html") {
-            $.getJSON("http://grnsight.cs.lmu.edu/beta/server/grnmap?path=download", function (uploadCount) {
+            $.getJSON("http://grnsight.cs.lmu.edu/beta/server/grnmap?path=download", function (downloadCount) {
                 $(".ga-download").text(downloadCount);
             })
         }
