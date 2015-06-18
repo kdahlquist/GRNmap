8bd7fadc2cdee3099724b8657e3802af2561d969
Trixie Roque <troque@lion.lmu.edu>
2015-06-18 01:45:04 -0700

    Changed some variables in ga-report.js

--- js/ga-report.js
+++ js/ga-report.js
@@ -2,8 +2,8 @@ $(function () {
     $.getJSON("http://grnsight.cs.lmu.edu/beta/server/grnmap?path=" + location.pathname.split("/").pop(), function (result) {
         
         if (location.pathname.split("/").pop() === "downloads.html") {
-            var exec = $(this).closest('div[id$="_executable"]').attr('id');
-            var source = $(this).closest('div[id$="_source"]').attr('id');
+            var exec = $('div[id$="_executable"]').attr('id');
+            var source = $('div[id$="_source"]').attr('id');
             $.getJSON("http://grnsight.cs.lmu.edu/beta/server/grnmap?path=download" + "/" + exec, function (execDownloadCount) {
                 $(".ga-execDownload").text(execDownloadCount);
             })
