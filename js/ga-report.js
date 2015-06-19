f7e30800def52c507aa80acf39d9712406c62ae0
dondi <dondi@lmu.edu>
2015-06-18 23:29:07 -0700

    Separate download report behavior.

--- js/ga-report.js
+++ js/ga-report.js
@@ -1,36 +1,8 @@
 $(function () {
-    var pathTail = location.pathname.split("/").pop();
-
-    var updateTotal = function ($total, subtotal) {
-            var current = +$total.text();
-            $total.text(current + subtotal);
-        };
-
-    var retrieveVersionCount = function ($container, subselector, $total) {
-            $.getJSON(
-                "http://grnsight.cs.lmu.edu/beta/server/grnmap?path=download/" + $container.attr('id'),
-                function (count) {
-                    $container.find(subselector).text(count);
-                    updateTotal($total, +count);
-                }
-            );
-        };
-
-    $.getJSON("http://grnsight.cs.lmu.edu/beta/server/grnmap?path=" + pathTail, function (result) {
-        
-        if (pathTail === "downloads.html") {
-            var $execTotal = $(".executable-total"),
-                $sourceTotal = $(".source-total");
-
-            $('div[id$="_executable"]').each(function (index, element) {
-                retrieveVersionCount($(element), ".ga-execDownload", $execTotal);
-            });
-
-            $('div[id$="_source"]').each(function (index, element) {
-                retrieveVersionCount($(element), ".ga-sourceDownload", $sourceTotal);
-            });            
+    $.getJSON(
+        "http://grnsight.cs.lmu.edu/beta/server/grnmap?path=" + location.pathname.split("/").pop(),
+        function (result) {
+            $(".ga-report").text(result);
         }
-
-        $(".ga-report").text(result);
-    });
+    );
 });
