b7958ec71407c2058b3fcec5052f345543535786
dondi <dondi@lmu.edu>
2015-06-18 23:20:04 -0700

    Implement download totals.

--- js/ga-report.js
+++ js/ga-report.js
@@ -1,11 +1,17 @@
 $(function () {
     var pathTail = location.pathname.split("/").pop();
 
-    var retrieveVersionCount = function ($container, subselector) {
+    var updateTotal = function ($total, subtotal) {
+            var current = +$total.text();
+            $total.text(current + subtotal);
+        };
+
+    var retrieveVersionCount = function ($container, subselector, $total) {
             $.getJSON(
                 "http://grnsight.cs.lmu.edu/beta/server/grnmap?path=download/" + $container.attr('id'),
                 function (count) {
                     $container.find(subselector).text(count);
+                    updateTotal($total, +count);
                 }
             );
         };
@@ -13,12 +19,15 @@ $(function () {
     $.getJSON("http://grnsight.cs.lmu.edu/beta/server/grnmap?path=" + pathTail, function (result) {
         
         if (pathTail === "downloads.html") {
+            var $execTotal = $(".executable-total"),
+                $sourceTotal = $(".source-total");
+
             $('div[id$="_executable"]').each(function (index, element) {
-                retrieveVersionCount($(element), ".ga-execDownload");
+                retrieveVersionCount($(element), ".ga-execDownload", $execTotal);
             });
 
             $('div[id$="_source"]').each(function (index, element) {
-                retrieveVersionCount($(element), ".ga-sourceDownload");
+                retrieveVersionCount($(element), ".ga-sourceDownload", $sourceTotal);
             });            
         }
 
