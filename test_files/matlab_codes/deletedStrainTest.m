5bb83903c224ddd44f8554a0711528d776615a75
juansc <jcarril3@lion.lmu.edu>
2016-01-22 16:58:21 -0800

    Beginning of test for deletion feature.

---
+++ test_files/matlab_codes/deletedStrainTest.m
@@ -0,0 +1,20 @@
+classdef deletedStrainTest < matlab.unittest.TestCase
+
+    properties
+        test_dir = '..\deleted_strain_tests\'
+    end
+    
+    methods (Test)
+        function testIdentifiesDeletedStrainWithLowerCaseNames(testCase)
+            
+        end
+        
+        function testIdentifiesDeletedStrainWithUpperCaseNames(testCase)
+        
+        end
+        
+        function testIdentifiesDeletedStrainWithMixedCaseNames(testCase)
+        
+        end
+    end
+end
