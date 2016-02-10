dd77e5ba75338a38a29d35b4d7c96ca3f5d319a6
trixr4kdz <troque@lion.lmu.edu>
2016-02-09 21:43:28 -0800

    Added new colors.

--- matlab/graphs.m
+++ matlab/graphs.m
@@ -22,7 +22,22 @@ tmax = max(expression_timepoints);
 figHandles  = findobj('Type','figure');
 offset      = size(figHandles,1);
 
-plot_colors = 'gkbrcm';
+
+
+plot_colors = [
+    0.6510    0.8078    0.8902;
+    0.1216    0.4706    0.7059;
+    0.6980    0.8745    0.5412;
+    0.2000    0.6275    0.1725;
+    0.9843    0.6039    0.6000;
+    0.8902    0.1020    0.1098;
+    0.9922    0.7490    0.4353;
+    1.0000    0.4980         0;
+    0.7922    0.6980    0.8392;
+    0.4157    0.2392    0.6039;
+    1.0000    1.0000    0.6000;
+    0.6941    0.3490    0.1569;   
+];
 
 if length(Strain) == 1
     Targets = {[Strain{1} ' data'],[Strain{1} ' model']};
@@ -44,8 +59,8 @@ for qq = 1:length(Strain)
         error_dn = (log2FC(qq).avg - 1.96*log2FC(qq).stdev);
         for ii=1:GRNstruct.GRNParams.num_genes
             figure(ii+offset),hold on
-            plot(td,log2FC(qq).data(ii+1,:),[plot_colors(qq) 'o'],'LineWidth',3),axis([tmin tmax -3 3]);
-            plot(log2FC(qq).simulation_timepoints,log2FC(qq).model(ii,:),[plot_colors(qq) '-']);
+            plot(td,log2FC(qq).data(ii+1,:),'o','Color',plot_colors(qq,:),'LineWidth',3),axis([tmin tmax -3 3]);
+            plot(log2FC(qq).simulation_timepoints,log2FC(qq).model(ii,:),'-','Color',plot_colors(qq,:));
             legend(Targets,'Location','NorthEastOutside');
             title(GRNstruct.labels.TX1{1+(ii),1},'FontSize',16)
             xlabel('Time (minutes)','FontSize',16)
