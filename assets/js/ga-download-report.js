23969e572c5e4c5d7ed6c888e7b57f0b8d9c2dfb
cazinge <eddie.azinge@gmail.com>
2016-09-17 15:22:05 -0700

    Converting site structure to basic Jekyll Structure

---
+++ assets/js/ga-download-report.js
@@ -0,0 +1,23 @@
+$(function () {
+    var updateTotal = function ($total, subtotal) {
+            $total.text(+$total.text() + subtotal);
+        };
+
+    var retrieveVersionCount = function ($container, subselector, $total) {
+            $.getJSON(
+                "http://grnsight.cs.lmu.edu/server/grnmap?path=download/" + $container.attr('id'),
+                function (count) {
+                    $container.find(subselector).text(count);
+                    updateTotal($total, +count);
+                }
+            );
+        };
+
+    $('div[id$="_executable"]').each(function (index, element) {
+        retrieveVersionCount($(element), ".ga-execDownload", $(".executable-total"));
+    });
+
+    $('div[id$="_source"]').each(function (index, element) {
+        retrieveVersionCount($(element), ".ga-sourceDownload", $(".source-total"));
+    });            
+});
