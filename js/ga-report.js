d3a461dbaa8713c75b3a4a1f668199c1bb531e5f
cazinge <eddie.azinge@gmail.com>
2016-09-17 15:26:36 -0700

    Removing previous file structure

--- js/ga-report.js
+++
@@ -1,8 +0,0 @@
-$(function () {
-    $.getJSON(
-        "http://grnsight.cs.lmu.edu/server/grnmap?path=" + location.pathname.split("/").pop(),
-        function (result) {
-            $(".ga-report").text(result);
-        }
-    );
-});
