709a5668a8e0130c714fcf6ef725e4020713aea1
Trixie Roque <troque@lion.lmu.edu>
2016-10-22 19:02:18 -0700

    Added assignments to populateGlobals function

--- matlab/populateGlobals.m
+++ matlab/populateGlobals.m
@@ -1,6 +1,11 @@
 function populateGlobals(GRNstruct)
-   global adjacency_mat b degrate deletion  fix_b num_genes prorate wts
+   global adjacency_mat b degrate  fix_b num_genes prorate
    
     adjacency_mat = GRNstruct.GRNParams.adjacency_mat;
+    b             = GRNstruct.GRNParams.b;
+    degrate       = GRNstruct.degrates;
+    fix_b         = GRNstruct.controlParams.fix_b;
+    num_genes     = GRNstruct.GRNParams.num_genes;
+    prorate       = GRNstruct.GRNParams.prorate;
 
 end
