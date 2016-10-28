e2ac70fc19aef8f4a87bafafe907521b7d8c79a8
Justin Kyle <jtorre39@lion.lmu.edu>
2016-10-28 12:16:35 -0700

    finished populate globals test and function and had to refactor constantgrnstructs further. broke some other test files going to look at those next

--- matlab/populateGlobals.m
+++ matlab/populateGlobals.m
@@ -1,7 +1,8 @@
 function populateGlobals(GRNstruct)
-   global adjacency_mat b degrate fix_b num_genes prorate production_function
+   global adjacency_mat alpha b degrate fix_b num_genes prorate production_function
    
     adjacency_mat       = GRNstruct.GRNParams.adjacency_mat;
+    alpha               = GRNstruct.GRNParams.alpha;
     b                   = GRNstruct.GRNParams.b;
     degrate             = GRNstruct.degRates;
     fix_b               = GRNstruct.controlParams.fix_b;
