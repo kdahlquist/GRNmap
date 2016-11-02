c6ab2ed7222c1ffc72d19047055ecf72943ef70f
Trixie Roque <troque@lion.lmu.edu>
2016-11-01 18:06:13 -0700

    Cleaned up test files

--- test_files/matlab_codes/initializeArraysTest.m
+++ test_files/matlab_codes/initializeArraysTest.m
@@ -10,6 +10,7 @@ classdef initializeArraysTest < matlab.unittest.TestCase
             addpath([pwd '/../../matlab']);
             testCase.GRNstruct.inputFile = [testCase.test_dir '4-genes_6-edges_artificial-data_MM_estimation_fixP-0_graph.xlsx'];
             testCase.GRNstruct = readInputSheet(testCase.GRNstruct);
+            testCase.GRNstruct = initializeArrays(testCase.GRNstruct);            
         end
     end
     
@@ -27,18 +28,15 @@ classdef initializeArraysTest < matlab.unittest.TestCase
 %         end
         
         function testInitialAndEstimatedGuessesSize(testCase)
-            testCase.GRNstruct = initializeArrays(testCase.GRNstruct);            
             testCase.verifyTrue(isequal(testCase.GRNstruct.locals.initial_guesses, [0; 0; 0; 0; 0; 0; 0; 0; 0; 0]));
             testCase.verifyTrue(isequal(testCase.GRNstruct.locals.estimated_guesses, [0; 0; 0; 0; 0; 0; 0; 0; 0; 0]));
         end
 
         function testGRNOutputSSESize(testCase)
-            testCase.GRNstruct = initializeArrays(testCase.GRNstruct);            
             testCase.verifyTrue(isequal(testCase.GRNstruct.GRNOutput.SSE, [0 0; 0 0; 0 0; 0 0]));
         end
         
         function testGRNOutputDSize(testCase)
-            testCase.GRNstruct = initializeArrays(testCase.GRNstruct);            
             testCase.verifyTrue(isequal(testCase.GRNstruct.GRNOutput.d, [0 0 0 0 0 0 0 0 0 0 0 0; ...
                                                                          0 0 0 0 0 0 0 0 0 0 0 0; ...
                                                                          0 0 0 0 0 0 0 0 0 0 0 0; ...
@@ -46,42 +44,34 @@ classdef initializeArraysTest < matlab.unittest.TestCase
         end
         
         function testGRNOutputProrateSize(testCase)
-            testCase.GRNstruct = initializeArrays(testCase.GRNstruct);            
             testCase.verifyTrue(isequal(testCase.GRNstruct.GRNOutput.prorate, [0; 0; 0; 0]));
         end
         
         function testGRNOutputDegrateSize(testCase)
-            testCase.GRNstruct = initializeArrays(testCase.GRNstruct);            
             testCase.verifyTrue(isequal(testCase.GRNstruct.GRNOutput.degrate, [0; 0; 0; 0]));
         end
         
         function testGRNOutputWtsSize(testCase)
-            testCase.GRNstruct = initializeArrays(testCase.GRNstruct);            
             testCase.verifyTrue(isequal(testCase.GRNstruct.GRNOutput.wts, [0; 0; 0; 0; 0 ;0]));
         end
         
         function testGRNOutputBSize(testCase)
-            testCase.GRNstruct = initializeArrays(testCase.GRNstruct);            
             testCase.verifyTrue(isequal(testCase.GRNstruct.GRNOutput.b, [0; 0; 0; 0]));
         end
         
         function testGRNOutputAdjacencyMatSize(testCase)
-            testCase.GRNstruct = initializeArrays(testCase.GRNstruct);            
             testCase.verifyTrue(isequal(testCase.GRNstruct.GRNOutput.adjacency_mat, [0 0 0 0; 0 0 0 0; 0 0 0 0; 0 0 0 0]));
         end
         
         function testGRNOutputActiveSize(testCase)
-            testCase.GRNstruct = initializeArrays(testCase.GRNstruct);            
             testCase.verifyTrue(isequal(testCase.GRNstruct.GRNOutput.active, [0 0 0 0]));
         end
         
         function testGRNOutputTspanSize(testCase)
-            testCase.GRNstruct = initializeArrays(testCase.GRNstruct);            
             testCase.verifyTrue(isequal(testCase.GRNstruct.GRNOutput.tspan, [0 0 0 0]));
         end
         
         function testGRNModelModelSize(testCase)
-            testCase.GRNstruct = initializeArrays(testCase.GRNstruct);    
             testCase.verifyTrue(isequal(testCase.GRNstruct.GRNModel(1).model, [0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0; ...
                                                                             0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0; ...
                                                                             0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0; ...
@@ -93,7 +83,6 @@ classdef initializeArraysTest < matlab.unittest.TestCase
         end
         
         function testGRNModelSimulationTimepointsSize(testCase)
-            testCase.GRNstruct = initializeArrays(testCase.GRNstruct);    
             testCase.verifyTrue(isequal(testCase.GRNstruct.GRNModel(1).simulation_timepoints, [0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0]));
             testCase.verifyTrue(isequal(testCase.GRNstruct.GRNModel(2).simulation_timepoints, [0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0]));
         end
