92e815c2a839b2eaf9bf119c0d9b4d4b3ac1ede7
Trixie Roque <troque@lion.lmu.edu>
2016-11-11 16:56:41 -0800

    Removed log2FC from runForwardSimulation and fixed globalToStruct routine

--- matlab/globalToStruct.m
+++ matlab/globalToStruct.m
@@ -3,25 +3,24 @@ function GRNstruct = globalToStruct(GRNstruct)
            degrate lse_out penalty_out SSE wts prorate log2FC
     
     GRNstruct.GRNOutput.adjacency_mat = adjacency_mat;
-    GRNstruct.GRNOutput.prorate = prorate;
-    GRNstruct.GRNOutput.degrate = degrate;
-    GRNstruct.GRNOutput.wts = wts;
-    GRNstruct.GRNOutput.b = b;
-    GRNstruct.GRNOutput.tspan = expression_timepoints;
-    GRNstruct.GRNOutput.alpha = alpha;
-    GRNstruct.GRNOutput.lse_out = lse_out;
-    GRNstruct.GRNOutput.SSE = SSE;
+    GRNstruct.GRNOutput.prorate       = prorate;
+    GRNstruct.GRNOutput.degrate       = degrate;
+    GRNstruct.GRNOutput.wts           = wts;
+    GRNstruct.GRNOutput.b             = b;
+    GRNstruct.GRNOutput.tspan         = expression_timepoints;
+    GRNstruct.GRNOutput.alpha         = alpha;
+    GRNstruct.GRNOutput.lse_out       = lse_out;
+    GRNstruct.GRNOutput.SSE           = SSE;
     
-    if GRNstruct.controlParams.estimate_params
-        GRNstruct.GRNOutput.reg_out = penalty_out;
-        GRNstruct.GRNOutput.counter = counter;
-    else
-        GRNstruct.GRNOutput.reg_out = NaN;
-        GRNstruct.GRNOutput.counter = 0;
+    for i = 1:length(GRNstruct.microData)
+        GRNstruct.GRNModel(i).model          = log2FC(i).model;
     end
     
-    GRNstruct.GRNModel.model = log2FC.model;
-
-%     We skipped log2FC due to too many parts
-%     GRNstruct.microData = log2FC; 
+    if GRNstruct.controlParams.estimate_params
+        GRNstruct.GRNOutput.reg_out   = penalty_out;
+        GRNstruct.GRNOutput.counter   = counter;
+    else
+        GRNstruct.GRNOutput.reg_out   = NaN;
+        GRNstruct.GRNOutput.counter   = 0;
+    end
 end
