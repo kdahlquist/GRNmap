414f649a20ef547a1c9291ea1f3dc0988c4bbbba
cazinge <eddie.azinge@gmail.com>
2016-11-30 11:15:48 -0800

    structured test suite and begun coding nested structure

--- matlab/compressMissingData.m
+++ matlab/compressMissingData.m
@@ -1,15 +1,10 @@
 function GRNstruct = compressMissingData (GRNstruct)
 
-    expression_timepoints = GRNstruct.controlParams.expression_timepoints;
-    % Naming new data expressionData
-
-    % Iterates through rawExpressionData(microData) for every
-    % unique timepoint compressing data into new data structure
-    currentTimePoint = 0;
-    currentTimePointIndex = 1;
-
     for index = 1:length(GRNStruct.Strain)
-        GRNstruct.expressionData(index).data = {};
+        GRNstruct.expressionData(index).data = convertToNestedStructure(...
+            GRNstruct.controlParams.expression_timepoints,...
+            GRNstruct.microData(index).data...
+        );
     end
 
 end
