e2dbb409155692898a197644d51b64b989d23258
Trixie Roque <troque@lion.lmu.edu>
2017-01-13 15:00:57 -0800

    Finishing up convertToNestedStructure with @jtorre39

--- matlab/convertToNestedStructure.m
+++ matlab/convertToNestedStructure.m
@@ -1,21 +1,34 @@
 function expressionData = convertToNestedStructure( timepoints, rawExpressionData )
 
-    expressionData = cell(size(rawExpresionData, 1), timepoints.t);
+    expressionData = cell(size(rawExpressionData, 1), size(timepoints, 1));
     
-    for index = 1:size(timepoints, 1)
-        expressionData{1, index} = timepoints.t(index);
+    % This takes care of heading (timepoints) for each column
+    for index = 1:size(timepoints, 2)
+        expressionData{1, index} = timepoints(index).t;
     end    
         
-    for row = 1:size(rawExpressionData, 1)
+    firstRowData = rawExpressionData (1, 1:end);
+    dataIndex = 1;
+    
+    for row = 2:size(rawExpressionData, 1)
+        dataMat = [];
+        expressionDataColumnCounter = 1;
         for col = 1:length(rawExpressionData)
-            % TODO: Nested Matrix Dissection
             
-%           if timepoints(2).indx = [4 5 6] -> [1 2 3] on expressionData
-%             dataIndex = mod( , col);
-            dataMat = [rawExpressionData(row, col); dataIndex];
-            if ~isnan(rawExpressionData(row, col))
-%                 expressionData{} = dataMat;
+            if col > 1 && firstRowData(1, col - 1) ~= firstRowData(1, col)
+                
+                expressionData{row, expressionDataColumnCounter} = dataMat;
+                expressionDataColumnCounter = expressionDataColumnCounter + 1;
+                
+                dataMat = [];
+                dataIndex = 1;
             end
+            if ~isnan(rawExpressionData(row, col))
+                currentDataMat = [rawExpressionData(row, col); dataIndex];
+                dataMat = [dataMat currentDataMat];
+            end
+            
+            dataIndex = dataIndex + 1;
         end 
     end
             
