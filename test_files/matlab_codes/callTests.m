134c762b85931a6008a22c29388bb9e892cda1a5
jtorre39 <jtorre39@lion.lmu.edu>
2018-02-23 22:23:50 -0800

    Fix for the output message to display failure properly.

--- test_files/matlab_codes/callTests.m
+++ test_files/matlab_codes/callTests.m
@@ -29,19 +29,23 @@ onlyShortTestsSuite = calculationSuite.selectIf( ...
     HasName(ContainsSubstring('computeStatisticsTest')) | ...
     HasName(ContainsSubstring('GeneralLSETest')));
 
-shortSuite = calculationSuite.selectIf(HasName(ContainsSubstring('computeStatisticsTest')));
+% shortSuite = calculationSuite.selectIf(HasName(ContainsSubstring('computeStatisticsTest')));
 % onlyReadInputSuite = excelSuite.selectIf(HasName(ContainsSubstring('ReadInputSheetTest')));
-
 allSuites = [dataStructureSuite, excelSuite, calculationSuite];
+
+% onlyShortTestSuite = calculationSuite.selectIf(HasName(ContainsSubstring('computeStatisticsTest')));
+% allSuites = onlyShortTestSuite;
+
 warning('on', 'convertToNestedStructure:SingleReplicateData');
 
 result = runner.run(allSuites);
 disp(result);
 resultBool = result;
 
+failure = 0;
 % check for any failures
 for i = size(result, 2)
-    if result(1, i).Failed == 0
+    if result(1, i).Failed == 1
         failure = 1;
         break
     end
