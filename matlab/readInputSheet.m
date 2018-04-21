6475046f80d69575de34bb173e279023b124fd73
jtorre39 <jtorre39@lion.lmu.edu>
2018-04-20 17:09:49 -0700

    Revert "Commented out code using MM and refactored tests accordingly."
    This reverts commit 94f18ef5a44dfd856b4b03368d70b7cefd7e1579.

--- matlab/readInputSheet.m
+++ matlab/readInputSheet.m
@@ -24,7 +24,7 @@ Strain = [];
 input_file = GRNstruct.inputFile;
 GRNstruct.expressionData = {};
 
-[parms0,parmnames0] = xlsread(input_file, 'optimization_parameters');
+[parms0,parmnames0] = xlsread(input_file,'optimization_parameters');
 [numRows,numCols] = size(parmnames0);
 
 % This part of the code reads the optimization parameter sheet and creates
@@ -118,16 +118,8 @@ GRNstruct.controlParams.L_curve                      = L_curve;
 if strcmpi(GRNstruct.controlParams.production_function, 'Sigmoid')
     [GRNstruct.GRNParams.b,GRNstruct.labels.TX6] = xlsread(input_file,'threshold_b');
 else
-    % Currently disabled until testing for Michaelis-Menten is completed!
-    % GRNstruct.controlParams.fix_b = 1;
-    % GRNstruct.GRNParams.b = zeros(length(GRNstruct.degRates),1);
-
-    % Instead we report a message to the user and error out.
-    msg = ['The Michaelis-Menten function is currently disabled until testing can '...
-            'be completed. Please select "Sigmoid" in order to run GRNmap.'];
-    msgbox(msg, 'warn');
-    ME = MException('readInputSheet:mmDetected', msg);
-    throw(ME)
+    GRNstruct.controlParams.fix_b = 1;
+    GRNstruct.GRNParams.b = zeros(length(GRNstruct.degRates),1);
 end
 
 %TX1 contains both the systemic and standard names
